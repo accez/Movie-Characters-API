d585521554e9a4b321e74956e3fb1ec5fe76f332
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-03-02 16:12:35 +0100

    feat: swagger and openai added

--- src/main/java/com/example/moviecharactersapi/Controllers/FranchiseController.java
+++ src/main/java/com/example/moviecharactersapi/Controllers/FranchiseController.java
@@ -4,6 +4,7 @@ package com.example.moviecharactersapi.Controllers;
 import com.example.moviecharactersapi.Models.Franchise;
 import com.example.moviecharactersapi.Repositorys.FranchiseRepository;
 import com.example.moviecharactersapi.Service.FranchiseService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,31 +25,34 @@ public class FranchiseController {
 
     // region CRUD
     // Create
+    @Operation(summary = "Create a franchise")
     @PostMapping("/")
     public Franchise addFranchise(@RequestBody Franchise franchise) {
         return repository.save(franchise);
     }
 
     // Read
+    @Operation(summary = "Get all franchises")
     @GetMapping("/")
     public Collection<Franchise> getAllFranchise() {
         return repository.findAll();
     }
 
+    @Operation(summary = "Get a franchise")
     @GetMapping("/{id}")
     public Franchise getFranchiseById(@PathVariable int id) {
         return repository.findById(id).orElse(null);
     }
 
     // Update
-
+    @Operation(summary = "Update a franchise")
     @PutMapping("/{id}")
     public Franchise updateFranchise(@PathVariable int id, @RequestBody Franchise franchise) {
         System.out.println(franchise.toString());
         franchise.setId(id);
         return repository.save(franchise);
     }
-
+    @Operation(summary = "Update a specific part of a franchise e.g the description")
     @PatchMapping("/{id}")
     public Franchise partialUpdateFranchise(@PathVariable int id, @RequestBody Franchise franchise) {
         franchise.setId(id);
@@ -56,6 +60,7 @@ public class FranchiseController {
     }
 
     // Delete
+    @Operation(summary = "Delete a franchise")
     @DeleteMapping("/{id}")
     public void deleteFranchise(@PathVariable int id){
         repository.deleteById(id);
--- src/main/java/com/example/moviecharactersapi/Controllers/MovieController.java
+++ src/main/java/com/example/moviecharactersapi/Controllers/MovieController.java
@@ -4,6 +4,7 @@ package com.example.moviecharactersapi.Controllers;
 import com.example.moviecharactersapi.Models.Movie;
 import com.example.moviecharactersapi.Repositorys.MovieRepository;
 import com.example.moviecharactersapi.Service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -12,8 +13,8 @@ import java.util.Collection;
 @RequestMapping("/movie")
 public class MovieController {
 
-    private MovieRepository repository;
-    private MovieService service;
+    final private MovieRepository repository;
+    final private MovieService service;
 
     public MovieController(MovieRepository repository, MovieService service){
         this.repository = repository;
@@ -23,29 +24,34 @@ public class MovieController {
 
     // region CRUD
     // CREATE
+    @Operation(summary = "Create a movie")
     @PostMapping("/")
     public Movie addMovie(@RequestBody Movie movie) {
         movie = repository.save(movie);
         return movie;
     }
     // READ
+    @Operation(summary = "Get all movies")
     @GetMapping("/")
     public Collection<Movie> getAllMOMovies() {
         return repository.findAll();
     }
 
+    @Operation(summary = "Get a movie")
     @GetMapping("/{id}")
     public Movie getMovieById(@PathVariable int id) {
         return repository.findById(id).orElse(null);
     }
 
     // UPDATE
+    @Operation(summary = "Update a movie")
     @PutMapping("/{id}")
     public Movie updateMovie(@PathVariable int id,@RequestBody Movie movie){
         movie.setId(id);
         return repository.save(movie);
     }
 
+    @Operation(summary = "Update a specific part of a movie e.g the title")
     @PatchMapping("/{id}")
     public Movie partialUpdateMovie(@PathVariable int id, @RequestBody Movie movie){
         movie.setId(id);
@@ -53,6 +59,7 @@ public class MovieController {
     }
 
     // DELETE
+    @Operation(summary = "Delete a movie")
     @DeleteMapping("/{id}")
     public void deleteMovie(@PathVariable int id) {
         repository.deleteById(id);
